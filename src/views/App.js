2525ca07b7abec67d3e8602ce36356a668b90c56
sellam abderrahmane <sellamabderrahmane3@gmail.com>
2021-08-22 11:47:06 +0100

    First commit

---
+++ src/views/App.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { About, Banner, CaseStudy, Education, Footer, Header, Specilizing } from "../layout/index"
+import { menus } from "../config"
+import PortfolioProvider from "./portfolio.context"
+
+function App() {
+  return (
+    <PortfolioProvider>
+      <div className='counter-scroll background-wrap fixed counter-scroll home4 has-one-page'>
+        <div id='mobile-menu-overlay' />
+        <Header menus={menus} />
+        <div id='content' className='content'>
+          <div className='homepage-personal'>
+            <Banner />
+            <About />
+            <Specilizing />
+            {/* <Project /> */}
+            <Education />
+            <CaseStudy />
+            <Footer menus={menus} />
+          </div>
+        </div>
+      </div>
+    </PortfolioProvider>
+  )
+}
+
+export default App
